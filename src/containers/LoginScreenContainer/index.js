1cdbb67d259ee540fd0ef6412b9d795e99b0e568
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2020-11-24 16:11:00 -0300

    TS09-TelaLogin (#7)
    * #1 - Adding template of page and fields style
    
    * #1 - Adding tests

--- src/containers/LoginScreenContainer/index.js
+++ src/containers/LoginScreenContainer/index.js
@@ -1,31 +1,32 @@
 import React from 'react';
-import {Redirect } from "react-router-dom";
+//import {Redirect } from "react-router-dom";
 
 import { Grid, TextField, Button } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
+//import axiosInstance from './../../auth/axiosApi.js'
+import {APPLICATION_RESET_PASSWORD_URL} from './../../api_urls';
+//import {DASHBOARD_PAGE_URL} from './../../api_urls';
 
-import axiosInstance from './../../auth/axiosApi.js'
-import {DASHBOARD_PAGE_URL,APPLICATION_RESET_PASSWORD_URL} from './../../api_urls';
-import {sendLoginRequest, verifyUserToken} from './sendLoginRequest'
+//import {sendLoginRequest, verifyUserToken} from './sendLoginRequest'
 import Alert from '@material-ui/lab/Alert';
+import CamaraLogoIcon from './../../assets/LoginScreenContainer/logo_camara.svg';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import IconButton from '@material-ui/core/IconButton';
+
 
 const useStyles = theme => ({
   body: {
-    backgroundColor: "#00AF82",
+    backgroundColor: "#121212",
     height: '100vh',
     fontFamily: 'Open Sans',
     position: 'fixed',
     display: 'flex',
     border: 0,
-  },
-  sidebar: {
-  	height: "5vh",
-  },
-  loginArea: {
-    height: '100vh',
-    backgroundColor: '#F2F2F2'
   },
   loginBox: {
     display: 'flex',
@@ -34,28 +35,21 @@ const useStyles = theme => ({
     alignItems: 'center',
     height: '40vh',
     marginTop: "12rem",
-  },
-  loginForm: {
-    margin: '1rem 0 0 0',
+    [theme.breakpoints.up('md')]: {
+      marginLeft:"6rem"
+    },
   },
   textField: {
+    color: 'white',
+    borderRadius: '4px 4px 0 0',
     margin: '1rem 0 0 0',
-    backgroundColor: 'white',
-    color: '#666',
+    backgroundColor: '#979797',
     "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
       borderColor: "#666"
     }
   },
-  prototipoArea: {
-    height: '100vh',
-    backgroundColor: '#F2F2F2',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: '0 20px 20px 0',
-  },
   loginButton: {
-    backgroundColor: '#C4C4C4',
+    backgroundColor: '#00C354',
     color: 'white',
     fontWeight: 'bold',
     '&:hover': {
@@ -63,21 +57,22 @@ const useStyles = theme => ({
     },
     justifyContent: 'flex-start',
   },
-  buttonArea: {
-    margin: '1rem 0 0 0',
-    display: 'flex',
-    justifyContent: 'space-between'
-  },
   forgotPassword: {
-    color: '#666',
-    textDecoration: 'none',
+    color: 'white',
     justifyContent: 'flex-end',
   },
   camaraLogo: {
     justifyContent: 'flex-end',
-    margin: '10rem 0 -10rem 0',
+    [theme.breakpoints.up('md')]: {
+      margin: '10rem 0 -10rem 0',
+    },
+    [theme.breakpoints.down('md')]: {
+      margin: '2rem 0 0 0',
+    },
   }
 });
+
+
 
 class LoginScreen extends React.Component {
   constructor(props){
@@ -94,26 +89,24 @@ class LoginScreen extends React.Component {
     };
     this.loginMethod = this.loginMethod.bind(this);
   }
- 
+ /*
   async verifyIfUserIsAlreadyLogged(){
-
     try{
       const result = await verifyUserToken( localStorage.getItem('access_token'));
-
       if(result.status===200){
         this.setState({succesfullLogin:true})
       }
     }catch(e){
       this.setState({succesfullLogin:false})
     }
-  }
+  }*/
 
   async loginMethod(event){
-    event.preventDefault();
+    //event.preventDefault();
     //const history = useHistory();
+  /*
     try{
       const result = await sendLoginRequest(this.state.email,this.state.password );
-
       if(result.status===200){
         axiosInstance.defaults.headers['Authorization'] = "JWT " + result.data.access;
         localStorage.setItem('access_token', result.data.access);
@@ -127,14 +120,14 @@ class LoginScreen extends React.Component {
       }else if(e.name === "Error"){
         this.setState({error:{status:true, message:" Email ou senha incorretos. Tente novamente."}})
       }
-    }
+    }*/
   }
 
   componentDidMount(){
     this._isMounted = true;
 
     if(this._isMounted){
-        this.verifyIfUserIsAlreadyLogged();
+        //this.verifyIfUserIsAlreadyLogged();
     }
 }
 
@@ -148,13 +141,16 @@ class LoginScreen extends React.Component {
     this.setState({password: e.target.value});
   };
 
+  handleClickShowPassword = () => {
+    this.setState({showPassword: !this.state.showPassword})
+  };
 
   render(){
     const { classes } = this.props;
 
-      if(this.state.succesfullLogin) {
+      /*if(this.state.succesfullLogin) {
         return <Redirect to={DASHBOARD_PAGE_URL}/>
-      }
+      }*/
       return (
         <div>
           {this.state.succesfullLogin ?
@@ -162,60 +158,52 @@ class LoginScreen extends React.Component {
             :
             <Grid container className={classes.body}>
               <div className={classes.loginArea}>
-                <Grid item xs={12} sm={6} md={6} className={classes.loginBox}>
-                    <Grid container item xs={6} sm={6} md={6} style={{display: 'flex', justifyContent: 'space-between'}}>
-                      <Box className="formItems" paddingTop={22} marginLeft={4}>
-                        <Grid item>
-                          <div>imagem logo</div>
-                        </Grid>
-                        <Grid item className={classes.loginForm}>
-                          {this.state.error.status ?
-                            <Alert severity="error">{this.state.error.message}</Alert>
-                          : null}     
-                          <TextField className={classes.textField} variant="outlined" value={this.state.email} placeholder="email" id="email" type="email" onChange={(e)=>{this.handleEmailFormChange(e)}} fullWidth autoFocus required />
-                          <TextField className={classes.textField} variant="outlined" value={this.state.password} placeholder="senha" id="password" type="password" onChange={(e)=>{this.handlePasswordFormChange(e)}} fullWidth required />
-                        </Grid>
-                        <Grid container className={classes.buttonArea}>
-                          <Grid item>
-                            <Button style= {{textTransform: 'capitalize'}} className={classes.loginButton} onClick={this.loginMethod} variant="contained">Acessar</Button>
-                          </Grid>
-                          <Grid item>
-                            <a href={APPLICATION_RESET_PASSWORD_URL} className={classes.forgotPassword}>Esqueci a senha </a>
-                          </Grid>
-                        </Grid>
-                      </Box>
-                      <div className="camaraLogo">
-                        <Grid item className={classes.camaraLogo}>
-                          <Box marginLeft={5} paddingTop={4}>
-                            <div>Logo camara dos deputados</div>
+                <Box className={classes.loginBox}>
+                    <Grid container  style={{display: 'flex', justifyContent: 'space-between'}}>
+                      <Box className="formItems" marginTop={2} marginX={2}>
+                        <Grid item xs={12} md={8}><Typography  variant="h1" style={{color:"#FFFFFF"}}>Dashboard de dados da participação popular</Typography></Grid>
+                        <Grid item xs={12} md={4}>
+                          <Box paddingTop={2} width="100%"  paddingBottom={2} >
+                            {this.state.error.status ? <Alert severity="error">{this.state.error.message}</Alert> : null}
+                            <TextField InputProps={{style: {color: "white"}}} className={classes.textField} variant="filled" value={this.state.email} label="Email" id="email" type="email" onChange={(e)=>{this.handleEmailFormChange(e)}} fullWidth autoFocus required />
+                            <TextField className={classes.textField} variant="filled" value={this.state.password} label="Senha" id="password" onChange={(e)=>{this.handlePasswordFormChange(e)}} fullWidth required type={this.state.showPassword ? 'text' : 'password'}
+                               InputProps={{
+                                  endAdornment: (
+                                    <InputAdornment position="end">
+                                      <IconButton aria-label="toggle password visibility" onClick={this.handleClickShowPassword} edge="end">
+                                        {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                                      </IconButton>
+                                    </InputAdornment>
+                                  ),
+                                  style:{color: "white"}
+                                }}/>
                           </Box>
                         </Grid>
-                      </div>
+                        <Grid item xs={12} md={4}>
+                          <Grid container>
+                            <Grid item xs={6}><Button style= {{textTransform: 'capitalize'}} className={classes.loginButton} onClick={this.loginMethod} variant="contained">Acessar</Button></Grid>
+                            <Grid item xs={6}>
+                              <Box display="flex" flexDirection="row-reverse"><a href={APPLICATION_RESET_PASSWORD_URL} className={classes.forgotPassword}>Esqueci a senha</a></Box>
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                        <Grid item className={classes.camaraLogo}>
+                          <img src={CamaraLogoIcon} alt="Logo da Câmara dos Deputados"/>
+                        </Grid>
+                      </Box>
                     </Grid>
-                </Grid>
+                </Box>
               </div>
-              <Grid item xs={12} sm={8} md={8}>
-                <div className={classes.prototipoArea}>
-                  <Box style={{margin: '5rem 0 0 0'}} width={1}>
-                    <Box display="flex" flexDirection="row-reverse" paddingRight={10}>
-                     <div>Logo prototipo</div>
-                    </Box>
-                  </Box>
-                </div>
-                
-              </Grid>
-              <Grid item xs={12} sm={1} md={1} className={classes.sidebar}></Grid>
             </Grid>
         }
         </div>
-
       )
   }
 
-  }
+}
 
-  LoginScreen.propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
+LoginScreen.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 export default withStyles(useStyles)(LoginScreen);
