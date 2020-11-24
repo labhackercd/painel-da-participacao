6064c053e022fe837ab14166441d7052adf48189
teo <teo@teos-MacBook-Pro.local>
2020-11-24 10:36:21 -0300

    #2 Correcting minor layout errors

--- src/components/Dashboard/NavBar/index.js
+++ src/components/Dashboard/NavBar/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
-import {AppBar, Tabs, Tab, Typography, Box, Grid, IconButton} from '@material-ui/core';
+import {AppBar, Tabs, Toolbar, Tab, Typography, Box, Grid, IconButton} from '@material-ui/core';
 import { ReactComponent as Logo }from './../../../assets/logo.svg';
 import { ReactComponent as LogoutIcon } from './../../../assets/user_logout_icon.svg';
 
@@ -17,11 +17,14 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.paper,
   },
   tab: {
-    height: '100%',
+    height: '90%',
     textTransform: 'capitalize',
+    margin: '0.5rem 0 0 0',
   },
   logo: {
     marginLeft: theme.spacing(2),
+  },
+  logout: {
   }
 }));
 
@@ -43,33 +46,35 @@ export default function NavBar() {
   return (
      <div className={classes.root}>
        <AppBar position="static">
-       <Grid container>
-          <Grid item md={2}>
-            <div className={classes.logo}>
-              <IconButton>
-                <Logo />
-              </IconButton>
-            </div>
-          </Grid>
-          <Grid item md={9}>
-            <Tabs
-                value={value}
-                onChange={handleChange}
-                TabIndicatorProps={{style: {background: '#00C354'}}}
-                className={classes.tab}>
-              <CustomTab id="tabs-1" label="Dashboard" {...a11yProps(0)} />
-              <CustomTab id="tabs-2" label="Configurações" {...a11yProps(1)} />
-              <CustomTab id="tabs-3" label="Documentação" {...a11yProps(2)} />
-            </Tabs>
-          </Grid>
-          <Grid item md={1}>
-            <div className={classes.logout}>
-              <IconButton>
-                <LogoutIcon />
-              </IconButton>
-            </div>
-          </Grid>
-       </Grid>
+        <Toolbar>
+           <Grid container>
+              <Grid item md={2}>
+                <div className={classes.logo}>
+                  <IconButton>
+                    <Logo />
+                  </IconButton>
+                </div>
+              </Grid>
+              <Grid item md={9}>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    TabIndicatorProps={{style: {background: '#00C354'}}}
+                    className={classes.tab}>
+                  <CustomTab id="tabs-1" label="Dashboard" {...a11yProps(0)} />
+                  <CustomTab id="tabs-2" label="Configurações" {...a11yProps(1)} />
+                  <CustomTab id="tabs-3" label="Documentação" {...a11yProps(2)} />
+                </Tabs>
+              </Grid>
+              <Grid item md={1}>
+                <div className={classes.logout}>
+                  <IconButton >
+                    <LogoutIcon />
+                  </IconButton>
+                </div>
+              </Grid>
+           </Grid>
+        </Toolbar>
        </AppBar>
      </div>
    );
--- src/containers/DashboardContainer/index.js
+++ src/containers/DashboardContainer/index.js
@@ -4,6 +4,13 @@ import Box from '@material-ui/core/Box';
 import { withRouter } from "react-router";
 import NavBar from './../../components/Dashboard/NavBar';
 
+const useStyles = theme => ({
+  body: {
+    border: 0,
+    width: '100%',
+  },
+});
+
 class Dashboard extends React.Component {
   constructor(props){
     super(props);
@@ -11,28 +18,20 @@ class Dashboard extends React.Component {
       dataLoaded: false,
     };
   }
-
-
   componentDidMount(){
       this._isMounted = true;
-
       if(this._isMounted){
           //console.log("Loaded")
       }
   }
-
   render(){
-
-
+    const classes = useStyles();
     return (
-      <div>
-      <NavBar></NavBar>
-          Dashboard
+      <div className={classes.body}>
+        <NavBar></NavBar>
       </div>
 		)
   }
 }
-
-
 //export default withRouter(withStyles(useStyles2)(Dashboard));
 export default withRouter(Dashboard);
