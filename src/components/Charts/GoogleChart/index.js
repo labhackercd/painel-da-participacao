c4500cbfb1debac35686085a13f146bd3b4ed852
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2020-12-15 18:42:46 -0300

    TS42-RefatorarGraficos (#32)
    * #30 - Adding google chart component and tests
    
    * #30 - Removing old charts code and refactoring audiencias code

--- src/components/Charts/GoogleChart/index.js
+++ src/components/Charts/GoogleChart/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import Chart from "react-google-charts";
 
 
-export default function CurveLineChart(props) {
+/*
+Type Charts
+ - LineChart
+ - BarChart
+ - Calendar -> CalendarHeatChart
+ - LineChart -> CurveLineChart with props options
+ - PieChart
+ - BarChart -> StackedBar with props options
+ - TreeMap
+*/
+
+export default function GoogleChart(props) {
   return (
     <Chart
       width={'500px'}
       height={'300px'}
-      chartType="LineChart"
+      chartType={props.chartType}
       loader={<div>Carregando Gráfico</div>}
       data={props.data}
       options={props.options}
