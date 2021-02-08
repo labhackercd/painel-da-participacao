cb0b72c5dfd5492929d1fd10c5785d719032fd42
Joao Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2021-02-08 16:30:16 -0300

    #41 - Changing google charts to receive props and add pie chart

--- src/components/Charts/GoogleChart/index.js
+++ src/components/Charts/GoogleChart/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Chart } from 'react-google-charts';
 
 /*
@@ -13,42 +14,24 @@ Type Charts
  - TreeMap
 */
 // export default function GoogleChart(chartType, data, options) {
-export default function GoogleChart() {
+export default function GoogleChart(props) {
+  const { chartType, data, options } = props;
+
   return (
     <Chart
       width="100%"
       height="100%"
-      chartType="ColumnChart"
+      chartType={chartType}
       loader={<div>Loading Chart</div>}
-      data={[
-        ['Data', 'Perguntas', 'Votos nas Perguntas', 'Mensagens do chat'],
-        ['01/12', 300, 800, 231],
-        ['02/12', 345, 545, 265],
-        ['03/12', 240, 865, 212],
-        ['04/12', 256, 870, 234],
-        ['05/12', 210, 856, 275],
-        ['06/12', 323, 822, 276],
-        ['07/12', 356, 762, 212],
-        ['08/12', 121, 542, 434],
-        ['09/12', 130, 232, 234],
-        ['10/12', 213, 212, 954],
-        ['11/12', 365, 309, 545],
-        ['12/12', 313, 312, 576],
-        ['13/12', 376, 376, 603],
-        ['14/12', 309, 354, 565],
-        ['15/12', 354, 323, 732],
-      ]}
-      options={{
-        bars: 'vertical',
-        legend: { position: 'top', maxLines: 3, textStyle: { color: 'white' } },
-        isStacked: 'true',
-        colors: ['#76480F', '#9E5E0D', '#DA7F0B'],
-        bar: { groupWidth: '40%' },
-        hAxis: { textStyle: { color: '#FFFFFF' } },
-        vAxis: { minValue: 0, gridlines: { color: 'transparent' }, textStyle: { color: '#FFFFFF' } },
-        backgroundColor: '#000000',
-      }}
+      data={data}
+      options={options}
       rootProps={{ 'data-testid': '3' }}
     />
   );
 }
+
+GoogleChart.propTypes = {
+  chartType: PropTypes.string.isRequired,
+  data: PropTypes.array.isRequired,
+  options: PropTypes.node.isRequired,
+};
