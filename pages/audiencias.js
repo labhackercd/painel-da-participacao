848ec9622484dfb4ba1535196807fd4f55ce13a7
Joao Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2021-03-08 16:54:46 -0300

    #53 - Removing sidebar and adding env variable to getStaticProps of audiencias page

--- pages/audiencias.js
+++ pages/audiencias.js
@@ -1,10 +1,10 @@
+/* eslint-disable no-await-in-loop */
 /* eslint-disable react/prop-types */
 import React from 'react';
 // import PropTypes from 'prop-types';
 import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Container } from '@material-ui/core';
-import HomeMenu from '../src/components/HomeMenu';
 import Layout from '../layouts/index';
 
 import Audiencias from '../src/containers/Audiencias';
@@ -34,69 +34,40 @@ function AudienciasPage({ dados }) {
   const classes = useStyles();
   return (
     <Layout value={0}>
-      <div className={classes.body}>
-        <Grid container className={classes.root}>
-          <HomeMenu />
-          <Container className={classes.container}>
-            <main className={classes.content}>
-              <Audiencias responseDataRanking={dados} />
-            </main>
-          </Container>
-        </Grid>
-      </div>
+      <Grid container className={classes.root}>
+        <Container className={classes.container}>
+          <main className={classes.content}>
+            <Audiencias responseDataRanking={dados} />
+          </main>
+        </Container>
+      </Grid>
     </Layout>
   );
 }
-export async function getStaticProps() {
-  const baseUrl = 'https://tes.edemocracia.camara.leg.br/audiencias/reports/api/ranking/?limit=1000&offset=1000';
-  const resp = await axios.get(`${baseUrl}`);
-  const dados = resp.data.results;
 
-  return {
-    props: {
-      dados,
-    },
-    revalidate: 600, // Update data every 10 minutes (600 seconds)
-  };
-}
-
-/*
 export async function getStaticProps() {
   let dados = [];
+
   async function getData() {
-    const baseUrl = 'https://edemocracia.camara.leg.br/audiencias/api/room/?is_visible=true&ordering=-created&page=';
-    // const baseUrl = 'https://tes.edemocracia.camara.leg.br/audiencias/reports/api/ranking/?limit=1000&offset=1000';
-    let page = 1;
-    const rooms = [];
-    // create a lastResult array which is going to be used to check if there is a next page
-    let lastResult = [];
-    do {
-      try {
-        // eslint-disable-next-line no-await-in-loop
-        const resp = await axios.get(`${baseUrl}${page}`);
+    const results = [];
+    // let url = 'https://tes.edemocracia.camara.leg.br/audiencias/reports/api/ranking/?limit=500';
+    let url = `${process.env.NEXT_PUBLIC_AUDIENCIAS_REPORT_RANKING_URL}?limit=500`;
 
-        lastResult = resp.data;
-        resp.data.results.forEach((room) => {
-          rooms.push(room);
-        });
-        page += 1;
-      } catch (err) {
-        return [];
-      }
-    } while (lastResult.next !== null);
-    return rooms;
+    try {
+      do {
+        const resp = await axios.get(url);
+        const data = await resp.data;
+        url = data.next;
+        results.push(...data.results);
+      } while (url);
+
+      return results;
+    } catch (err) {
+      return [];
+    }
   }
 
-  try {
-    // const url = 'https://edemocracia.camara.leg.br/audiencias/api/ranking/room';
-    // let url = 'https://edemocracia.camara.leg.br/audiencias/api/room/?ordering=-created&is_visible=true';
-    // const response = await axios.get(url);
-    // const dados = response.data.data;
-    dados = await getData();
-  } catch (e) {
-    console.log(e);
-    console.log('Deu ruim');
-  }
+  dados = await getData();
 
   return {
     props: {
@@ -105,6 +76,5 @@ export async function getStaticProps() {
     revalidate: 600, // Update data every 10 minutes (600 seconds)
   };
 }
-*/
 
 export default AudienciasPage;
