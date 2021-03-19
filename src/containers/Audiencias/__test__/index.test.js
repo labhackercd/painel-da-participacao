971af9d9f812f73c087796f97da012e7607496a6
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2021-03-19 16:53:55 -0300

    TS75-RefatorarAplicacaoPeriodo (#76)
    * #75 - Moving functions and components to other files
    
    * #75 - Adding radix parameter to require functions
    
    * #75 - Adding tests of auxFunctions file and mock files
    
    * #75 - Adding tests for check change of periods
    
    * #75 - Fixing eslint errors

--- src/containers/Audiencias/__test__/index.test.js
+++ src/containers/Audiencias/__test__/index.test.js
@@ -6,8 +6,18 @@ import MockAdapter from 'axios-mock-adapter';
 import MockTheme from '../../../components/mockTheme';
 import Audiencias from '../index';
 
-import newUsersMockResponse from './mocks/new_users_mock';
-import rankingRoomsMock from './mocks/ranking_rooms_mock';
+import newUsersMock from './mocks/new_users_mock';
+import dataRanking from './mocks/ranking_rooms_mock';
+
+import messagesRankingMock from './mocks/messages_ranking_mock';
+import participantesUsersMock from './mocks/participants_users_mock';
+import questionsRankingMock from './mocks/questions_ranking_mock';
+import roomsRankingMock from './mocks/rooms_ranking_mock';
+import votesRankingMock from './mocks/votes_ranking_mock';
+
+const defaultSearchQuery = '?period=monthly&start_date__year=2021&ordering=start_date';
+const yearlySearchQuery = '?period=yearly&ordering=start_date';
+const dailySearchQuery = '?period=daily&start_date__year=2021&start_date__month=4&ordering=start_date';
 
 test('snapshot should not have changes', () => {
   let component;
@@ -26,19 +36,185 @@ test('Test if Audiencias renders without crash whole lifecycle', () => {
   expect(wrapper.exists()).toEqual(true);
 });
 
-test('Test audiencias default page lifecycle with requests', async (done) => {
+test('Default page lifecycle is getting default informations of period by month of year 2021 and getting all years information ', async () => {
   const mockInstance = new MockAdapter(axios);
-  const period = 'yearly';
-  const urlNewUsersByYear = `${process.env.NEXT_PUBLIC_AUDIENCIAS_NEW_USERS_URL}?period=${period}&ordering=start_date`;
-  await mockInstance
-    .onGet(process.env.NEXT_PUBLIC_AUDIENCIAS_NEW_USERS_URL)
-    .reply(200, newUsersMockResponse)
-    .onGet(urlNewUsersByYear)
-    .reply(200, newUsersMockResponse)
-    .onGet(process.env.NEXT_PUBLIC_AUDIENCIAS_ROOMS_RANKING_URL)
-    .reply(200, rankingRoomsMock);
+  mockInstance
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_PARTICIPANT_USERS_URL}${defaultSearchQuery}`)
+    .reply(200, participantesUsersMock.MONTHLY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_ROOMS_RANKING_URL}${defaultSearchQuery}`)
+    .reply(200, roomsRankingMock.MONTHLY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_MESSAGES_RANKING_URL}${defaultSearchQuery}`)
+    .reply(200, messagesRankingMock.MONTHLY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_QUESTIONS_RANKING_URL}${defaultSearchQuery}`)
+    .reply(200, questionsRankingMock.MONTHLY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_VOTES_RANKING_URL}${defaultSearchQuery}`)
+    .reply(200, votesRankingMock.MONTHLY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_NEW_USERS_URL}${defaultSearchQuery}`)
+    .reply(200, newUsersMock.MONTHLY)
 
-  const wrapper = await mount(<MockTheme><Audiencias /></MockTheme>);
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_PARTICIPANT_USERS_URL}${yearlySearchQuery}`)
+    .reply(200, participantesUsersMock.YEARLY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_ROOMS_RANKING_URL}${yearlySearchQuery}`)
+    .reply(200, roomsRankingMock.YEARLY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_MESSAGES_RANKING_URL}${yearlySearchQuery}`)
+    .reply(200, messagesRankingMock.YEARLY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_QUESTIONS_RANKING_URL}${yearlySearchQuery}`)
+    .reply(200, questionsRankingMock.YEARLY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_VOTES_RANKING_URL}${yearlySearchQuery}`)
+    .reply(200, votesRankingMock.YEARLY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_NEW_USERS_URL}${yearlySearchQuery}`)
+    .reply(200, newUsersMock.YEARLY)
+
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_PARTICIPANT_USERS_URL}${dailySearchQuery}`)
+    .reply(200, participantesUsersMock.DAILY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_ROOMS_RANKING_URL}${dailySearchQuery}`)
+    .reply(200, roomsRankingMock.DAILY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_MESSAGES_RANKING_URL}${dailySearchQuery}`)
+    .reply(200, messagesRankingMock.DAILY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_QUESTIONS_RANKING_URL}${dailySearchQuery}`)
+    .reply(200, questionsRankingMock.DAILY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_VOTES_RANKING_URL}${dailySearchQuery}`)
+    .reply(200, votesRankingMock.DAILY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_NEW_USERS_URL}${dailySearchQuery}`)
+    .reply(200, newUsersMock.DAILY);
+
+  const wrapper = mount(
+    <MockTheme><Audiencias responseDataRanking={dataRanking} /></MockTheme>,
+  );
   wrapper.update();
-  done();
+
+  const yearPeriodSelect = wrapper.find('select').at(0);
+  yearPeriodSelect.instance().value = '0';
+  yearPeriodSelect.simulate('change');
+  expect(wrapper.find('select').at(0).prop('value')).toEqual('0');
+
+  const filterButton = wrapper.find('button').at(0);
+  filterButton.simulate('click');
+  await wrapper.update();
+
+  setImmediate(async () => {
+    await wrapper.update();
+
+    yearPeriodSelect.instance().value = '2021';
+    yearPeriodSelect.simulate('change');
+    expect(wrapper.find('select').at(0).prop('value')).toEqual('2021');
+
+    const monthPeriodSelect = wrapper.find('select').at(1);
+    monthPeriodSelect.instance().value = '4';
+    monthPeriodSelect.simulate('change');
+    expect(wrapper.find('select').at(1).prop('value')).toEqual('4');
+
+    filterButton.simulate('click');
+    await wrapper.update();
+
+    setImmediate(async () => {
+      await wrapper.update();
+    });
+  });
+});
+
+test('Audiencia page lifecycle is getting informations of days of specific month', async () => {
+  const mockInstance = new MockAdapter(axios);
+  mockInstance
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_PARTICIPANT_USERS_URL}${defaultSearchQuery}`)
+    .reply(200, participantesUsersMock.MONTHLY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_ROOMS_RANKING_URL}${defaultSearchQuery}`)
+    .reply(200, roomsRankingMock.MONTHLY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_MESSAGES_RANKING_URL}${defaultSearchQuery}`)
+    .reply(200, messagesRankingMock.MONTHLY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_QUESTIONS_RANKING_URL}${defaultSearchQuery}`)
+    .reply(200, questionsRankingMock.MONTHLY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_VOTES_RANKING_URL}${defaultSearchQuery}`)
+    .reply(200, votesRankingMock.MONTHLY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_NEW_USERS_URL}${defaultSearchQuery}`)
+    .reply(200, newUsersMock.MONTHLY)
+
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_PARTICIPANT_USERS_URL}${dailySearchQuery}`)
+    .reply(200, participantesUsersMock.DAILY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_ROOMS_RANKING_URL}${dailySearchQuery}`)
+    .reply(200, roomsRankingMock.DAILY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_MESSAGES_RANKING_URL}${dailySearchQuery}`)
+    .reply(200, messagesRankingMock.DAILY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_QUESTIONS_RANKING_URL}${dailySearchQuery}`)
+    .reply(200, questionsRankingMock.DAILY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_VOTES_RANKING_URL}${dailySearchQuery}`)
+    .reply(200, votesRankingMock.DAILY)
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_NEW_USERS_URL}${dailySearchQuery}`)
+    .reply(200, newUsersMock.DAILY);
+
+  const wrapper = mount(
+    <MockTheme><Audiencias responseDataRanking={dataRanking} /></MockTheme>,
+  );
+  wrapper.update();
+
+  const yearPeriodSelect = wrapper.find('select').at(0);
+  yearPeriodSelect.instance().value = '2021';
+  yearPeriodSelect.simulate('change');
+  expect(wrapper.find('select').at(0).prop('value')).toEqual('2021');
+
+  const monthPeriodSelect = wrapper.find('select').at(1);
+  monthPeriodSelect.instance().value = '4';
+  monthPeriodSelect.simulate('change');
+  expect(wrapper.find('select').at(1).prop('value')).toEqual('4');
+
+  const filterButton = wrapper.find('button').at(0);
+  filterButton.simulate('click');
+  await wrapper.update();
+
+  setImmediate(async () => {
+    await wrapper.update();
+  });
+});
+
+test('Audiencia page lifecycle is getting informations with no values', async () => {
+  const mockInstance = new MockAdapter(axios);
+  mockInstance
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_PARTICIPANT_USERS_URL}${defaultSearchQuery}`)
+    .reply(200, [])
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_ROOMS_RANKING_URL}${defaultSearchQuery}`)
+    .reply(200, [])
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_MESSAGES_RANKING_URL}${defaultSearchQuery}`)
+    .reply(200, [])
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_QUESTIONS_RANKING_URL}${defaultSearchQuery}`)
+    .reply(200, [])
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_VOTES_RANKING_URL}${defaultSearchQuery}`)
+    .reply(200, [])
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_NEW_USERS_URL}${defaultSearchQuery}`)
+    .reply(200, [])
+
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_PARTICIPANT_USERS_URL}${dailySearchQuery}`)
+    .reply(200, [])
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_ROOMS_RANKING_URL}${dailySearchQuery}`)
+    .reply(200, [])
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_MESSAGES_RANKING_URL}${dailySearchQuery}`)
+    .reply(200, [])
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_QUESTIONS_RANKING_URL}${dailySearchQuery}`)
+    .reply(200, [])
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_VOTES_RANKING_URL}${dailySearchQuery}`)
+    .reply(200, [])
+    .onGet(`${process.env.NEXT_PUBLIC_AUDIENCIAS_NEW_USERS_URL}${dailySearchQuery}`)
+    .reply(200, []);
+
+  const wrapper = await mount(
+    <MockTheme><Audiencias responseDataRanking={[]} /></MockTheme>,
+  );
+  await wrapper.update();
+
+  const yearPeriodSelect = wrapper.find('select').at(0);
+  yearPeriodSelect.instance().value = '2021';
+  yearPeriodSelect.simulate('change');
+  expect(wrapper.find('select').at(0).prop('value')).toEqual('2021');
+
+  const monthPeriodSelect = wrapper.find('select').at(1);
+  monthPeriodSelect.instance().value = '4';
+  monthPeriodSelect.simulate('change');
+  expect(wrapper.find('select').at(1).prop('value')).toEqual('4');
+
+  const filterButton = wrapper.find('button').at(0);
+  filterButton.simulate('click');
+  await wrapper.update();
+
+  setImmediate(async () => {
+    await wrapper.update();
+  });
 });
---
+++ src/containers/Audiencias/auxComponentes.js
@@ -0,0 +1,161 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Typography } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import ChartDataFrame from '../../components/ChartDataFrame/index';
+import TotalsDataFrame from '../../components/TotalsDataFrame/index';
+import GoogleChart from '../../components/Charts/GoogleChart';
+import Tooltip from '../../components/ToolTip/index';
+
+export function TotalFrame(props) {
+  const {
+    isLoaded, info, title, toolTipText,
+  } = props;
+
+  return (
+    <TotalsDataFrame height="15vh" paddingLeft="0.5rem" title={title} download={false} align="left" toolTipText={toolTipText}>
+      {isLoaded ? (
+        <Typography variant="h2" style={{ color: '#FFF', alignSelf: 'center' }}>
+          {info}
+        </Typography>
+      ) : (
+        <Box display="flex" alignItems="center" justifyContent="center" width="100%" height="100%">
+          <CircularProgress color="secondary" />
+        </Box>
+      )}
+    </TotalsDataFrame>
+  );
+}
+
+export function GoogleChartFrame(props) {
+  const {
+    isLoaded, title, classes, data, chartType, chartOptions, exportData,
+  } = props;
+
+  return (
+    <>
+      <ChartDataFrame height="35vh" title={title} listView exportData={exportData} download align="center">
+        {isLoaded ? (
+          <div className={classes.contentBox}>
+            <GoogleChart
+              chartType={chartType}
+              data={data}
+              options={chartOptions}
+            />
+          </div>
+        ) : (
+          <Box display="flex" alignItems="center" justifyContent="center" width="100%" height="100%">
+            <CircularProgress color="secondary" />
+          </Box>
+        )}
+      </ChartDataFrame>
+    </>
+  );
+}
+
+export function NoDataForSelectedPeriod(props) {
+  const { title } = props;
+
+  return (
+    <ChartDataFrame height="10vh" title={title} listView exportData={null} download={false}>
+      <Box display="flex" alignItems="center" justifyContent="center" width="100%" height="100%">
+        <Typography>Não existem dados para o período selecionado</Typography>
+      </Box>
+    </ChartDataFrame>
+  );
+}
+
+export function Sectionheader(props) {
+  const { title, toolTipText } = props;
+
+  return (
+    <Box display="flex" marginBottom={1}>
+      <Box p={1}>
+        <Typography component="div">
+          <Box fontWeight="fontWeightBold" fontSize={39}>
+            {title}
+          </Box>
+        </Typography>
+      </Box>
+      <Box alignSelf="center">
+        {(toolTipText !== null && toolTipText !== undefined)
+          && <Tooltip toolTipText={toolTipText} />}
+      </Box>
+      <Box p={1} flexGrow={1} alignSelf="center">
+        <hr style={{ borderColor: '#DA7F0B' }} />
+      </Box>
+    </Box>
+  );
+}
+
+export function SubSectionHeader(props) {
+  const { title } = props;
+  return (
+    <Typography component="div">
+      <Box fontWeight="fontWeightBold" fontSize={25} marginLeft={1} marginBottom={1}>
+        {title}
+      </Box>
+    </Typography>
+  );
+}
+
+TotalFrame.propTypes = {
+  isLoaded: PropTypes.bool,
+  info: PropTypes.node,
+  title: PropTypes.string,
+  toolTipText: PropTypes.string,
+};
+
+TotalFrame.defaultProps = {
+  isLoaded: false,
+  info: 'info',
+  title: 'Title',
+  toolTipText: null,
+};
+
+GoogleChartFrame.propTypes = {
+  isLoaded: PropTypes.bool,
+  classes: PropTypes.object,
+  title: PropTypes.string,
+  data: PropTypes.node,
+  chartType: PropTypes.string,
+  chartOptions: PropTypes.object,
+  exportData: PropTypes.array,
+};
+
+GoogleChartFrame.defaultProps = {
+  isLoaded: false,
+  classes: '',
+  title: 'Title',
+  data: {},
+  chartType: '',
+  chartOptions: {},
+  exportData: [],
+};
+
+NoDataForSelectedPeriod.propTypes = {
+  title: PropTypes.string,
+};
+
+NoDataForSelectedPeriod.defaultProps = {
+  title: 'Titulo',
+};
+
+Sectionheader.propTypes = {
+  title: PropTypes.string,
+  toolTipText: PropTypes.string,
+};
+
+Sectionheader.defaultProps = {
+  title: 'Titulo',
+  toolTipText: null,
+};
+
+SubSectionHeader.propTypes = {
+  title: PropTypes.string,
+};
+
+SubSectionHeader.defaultProps = {
+  title: 'Titulo',
+};
