0b735fad2d323da92fd47257c4f90d97ba35375a
Joao Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2021-03-31 11:13:25 -0300

    #83 - Fixing eslint errors

--- src/components/ChartDataFrame/index.js
+++ src/components/ChartDataFrame/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import {
-  Grid, Box, Typography, IconButton,
+  Grid, Box, Typography,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
