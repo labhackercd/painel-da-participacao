5846b911159d44307e102beca0b44f99aa83d4d0
Joao Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2021-04-14 16:57:22 -0300

    #97 - Adding tests

--- src/services/functions/auxFunctions/__test__/auxFunctions.test.js
+++ src/services/functions/auxFunctions/__test__/auxFunctions.test.js
@@ -1,5 +1,7 @@
 import {
   getParticipationChartDataByDay, getParticipationChartDataByMonth, getParticipationChartDataByYear,
+  getWikilegisParticipationChartDataByDay, getWikilegisParticipationChartDataByMonth,
+  getWikilegisParticipationChartDataByYear,
 } from '../index';
 
 import messagesRankingMock from './mocks/messages_ranking_mock';
@@ -51,6 +53,56 @@ describe('Test getParticipationChartDataByYear function', () => {
     const resultArray = await getParticipationChartDataByYear(
       messagesRankingMock.YEARLY.results, questionsRankingMock.YEARLY.results,
       votesRankingMock.YEARLY.results,
+      2016,
+    );
+    expect(resultArray).not.toBe(null);
+    expect(resultArray.length).not.toBe(1);
+    expect(resultArray[0][0]).toBe('2016');
+    expect(resultArray.length).toBe(6);
+  });
+});
+
+describe('Test getWikilegisParticipationChartDataByDay function', () => {
+  const testMonth = 4;
+  const testYear = 2021;
+
+  test('getWikilegisParticipationChartDataByDay return data with 30 days(april)', async () => {
+    const resultArray = await getWikilegisParticipationChartDataByDay(
+      testMonth, testYear,
+      messagesRankingMock.DAILY.results, questionsRankingMock.DAILY.results,
+    );
+    expect(resultArray.length).toBe(30);
+  });
+
+  test('getWikilegisParticipationChartDataByDay return data with 31 days(may)', async () => {
+    const resultArray = await getWikilegisParticipationChartDataByDay(
+      (testMonth + 1), testYear,
+      messagesRankingMock.DAILY.results, questionsRankingMock.DAILY.results,
+    );
+    expect(resultArray.length).toBe(31);
+  });
+});
+
+describe('Test getWikilegisParticipationChartDataByMonth function', () => {
+  const testMonth = 4;
+  const testYear = 2021;
+
+  test('getWikilegisParticipationChartDataByMonth return data with 12 months', async () => {
+    const resultArray = await getWikilegisParticipationChartDataByMonth(
+      testMonth, testYear,
+      messagesRankingMock.MONTHLY.results, questionsRankingMock.MONTHLY.results,
+    );
+
+    expect(resultArray).not.toBe(null);
+    expect(resultArray.length).not.toBe(1);
+    expect(resultArray.length).toBe(12);
+  });
+});
+
+describe('Test getWikilegisParticipationChartDataByYear function', () => {
+  test('getWikilegisParticipationChartDataByYear return data with 6 years', async () => {
+    const resultArray = await getWikilegisParticipationChartDataByYear(
+      messagesRankingMock.YEARLY.results, questionsRankingMock.YEARLY.results,
       2016,
     );
     expect(resultArray).not.toBe(null);
