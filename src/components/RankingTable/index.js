00e891f06a2e0b04e0fa36928e8d7f32d400b159
Joao Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2021-04-20 15:55:27 -0300

    #109 - Adding search input layout

--- src/components/RankingTable/index.js
+++ src/components/RankingTable/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import DataTable, { createTheme } from 'react-data-table-component';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import SearchIcon from '@material-ui/icons/Search';
 
 createTheme('darkLAB', {
   text: {
@@ -42,21 +49,80 @@ createTheme('darkLAB', {
   },
 });
 
+const useStyles = makeStyles(() => ({
+  root: {
+    '& .MuiInput-underline:before': {
+      borderBottomColor: 'white',
+    },
+    '& label.Mui-focused': {
+      color: 'white',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: 'white',
+    },
+    marginRight: 15,
+  },
+  input: {
+    '&::placeholder': {
+      color: 'white',
+      fontSize: 14,
+      textOverflow: 'ellipsis !important',
+    },
+  },
+  textField: {
+    color: 'white',
+  },
+  button: {
+    backgroundColor: '#C4C4C4',
+    marginRight: 2,
+    textTransform: 'none',
+  },
+
+}));
+
 export default function RankingTable(props) {
   const { data, columns } = props;
+  const classes = useStyles();
 
   return (
-    <DataTable
-      columns={columns}
-      data={data}
-      theme="darkLAB"
-      highlightOnHover
-      pointerOnHover
-      pagination
-      paginationRowsPerPageOptions={[10, 50, 100, 1000]}
-      defaultSortField="participants_count"
-      defaultSortAsc={false}
-    />
+    <>
+      <Box display="flex" marginTop={2} marginRight={2} justifyContent="flex-end">
+        <FormControl className={classes.root}>
+          <TextField
+            id="standard-search"
+            type="search"
+            placeholder="Pesquise aqui"
+            InputLabelProps={{ className: classes.textField }}
+            InputProps={{
+              classes: {
+                input: classes.input,
+              },
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </FormControl>
+        <Button variant="contained" size="small" className={classes.button}>
+          Buscar
+        </Button>
+
+      </Box>
+
+      <DataTable
+        columns={columns}
+        data={data}
+        theme="darkLAB"
+        highlightOnHover
+        pointerOnHover
+        pagination
+        paginationRowsPerPageOptions={[10, 50, 100, 1000]}
+        defaultSortField="participants_count"
+        defaultSortAsc={false}
+      />
+    </>
   );
 }
 
