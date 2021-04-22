bd0c9d8601ea7b54379896932a15cf76b03e78ea
Joao Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2021-04-22 16:58:55 -0300

    110 - Adding function to filter wikilegis ranking

--- src/containers/Wikilegis/settings.js
+++ src/containers/Wikilegis/settings.js
@@ -74,3 +74,21 @@ export const rankingWikilegisHeaders = [
   { label: 'Número de Participantes', key: 'participants_count' },
   { label: 'URL', key: 'get_absolute_url' },
 ];
+
+export function filterRankingWikilegis(data, searchedText) {
+  const filter = data.filter(
+    (item) => (
+      (item.document.title ? item.document.title.toLowerCase() : '').includes(searchedText.toLowerCase())
+      || (item.openning_date ? formatDate(item.openning_date) : '').includes(searchedText.toLowerCase())
+      || (item.closing_date ? formatDate(item.closing_date) : '').includes(searchedText.toLowerCase())
+      || (item.suggestions_count ? item.suggestions_count.toString() : '').includes(searchedText.toLowerCase())
+      || (item.vote_count ? item.vote_count.toString() : '').includes(searchedText.toLowerCase())
+      || (item.participants_count ? item.participants_count.toString() : '').includes(searchedText.toLowerCase())
+      || (item.votes_count ? item.votes_count.toString() : '').includes(searchedText.toLowerCase())
+      || (item.document.responsible.name.toLowerCase() ? item.document.responsible.name.toLowerCase() : '').includes(searchedText.toLowerCase())
+      || (item.document.responsible.party_initials.toLowerCase() ? item.document.responsible.party_initials.toLowerCase() : '').includes(searchedText.toLowerCase())
+      || (item.document.responsible.uf.toLowerCase() ? item.document.responsible.uf.toLowerCase() : '').includes(searchedText.toLowerCase())
+    ),
+  );
+  return filter;
+}
