d28f8be95a757fe95bd63e17869c4719f7c8c30c
Joao Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2021-04-30 12:18:05 -0300

    #118 - Adding custom loading page indicator when next router is changing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
+import Router from 'next/router';
+import NProgress from 'nprogress'; // nprogress module
+import 'nprogress/nprogress.css'; // styles of nprogress
+import { ThemeProvider, makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 import theme from '../styles/theme';
 
+const useStyles = makeStyles((themeDOM) => ({
+  backdrop: {
+    zIndex: themeDOM.zIndex.drawer + 1,
+    color: '#fff',
+  },
+}));
+
+// Binding events.
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 export default function MyApp(props) {
+  const classes = useStyles();
   const { Component, pageProps } = props;
+  const [loadingPage, setLoadingPage] = useState(false);
+
+  Router.events.on('routeChangeStart', () => setLoadingPage(true));
+  Router.events.on('routeChangeComplete', () => setLoadingPage(false));
+  Router.events.on('routeChangeError', () => setLoadingPage(false));
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
@@ -25,6 +49,9 @@ export default function MyApp(props) {
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
+        <Backdrop className={classes.backdrop} open={loadingPage}>
+          <CircularProgress color="inherit" />
+        </Backdrop>
       </ThemeProvider>
     </>
   );
