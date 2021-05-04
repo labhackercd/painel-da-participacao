7c9227e6bb5ceaab644edd14e516b416ecb98edd
Joao Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2021-05-04 15:46:35 -0300

    #124 - Fixing bug of total of users been computed in invalid dates

--- src/containers/Wikilegis/index.js
+++ src/containers/Wikilegis/index.js
@@ -156,44 +156,53 @@ function Wikilegis(props) {
     const computedArray = [];
     let collumPeriodTitle = [];
 
-    switch (period) {
-      case dailyKeyWord:
-        computedArray.push([values[0].start_date.match(/\d+/g)[2], values[0].new_users]);
-        for (let i = 1; i < values.length; i += 1) {
-          computedArray.push(
-            [values[i].start_date.match(/\d+/g)[2],
-              values[i].new_users + computedArray[i - 1][1]],
-          );
+    try {
+      if (values !== null) {
+        switch (period) {
+          case dailyKeyWord:
+            computedArray.push([values[0].start_date.match(/\d+/g)[2], values[0].new_users]);
+            for (let i = 1; i < values.length; i += 1) {
+              computedArray.push(
+                [values[i].start_date.match(/\d+/g)[2],
+                  values[i].new_users + computedArray[i - 1][1]],
+              );
+            }
+            collumPeriodTitle = [['Dia', 'Total de Usuários Cadastrados']];
+            break;
+          case monthlyKeyWord:
+            computedArray.push(
+              [monthNamesList[(new Date(values[0].end_date)).getMonth()], values[0].new_users],
+            );
+            for (let i = 1; i < values.length; i += 1) {
+              computedArray.push(
+                [monthNamesList[(new Date(values[i].end_date)).getMonth()],
+                  values[i].new_users + computedArray[i - 1][1]],
+              );
+            }
+            collumPeriodTitle = [['Mês', 'Total de Usuários Cadastrados']];
+            break;
+          default:
+            computedArray.push(
+              [new Date(values[0].end_date).getFullYear().toString(), values[0].new_users],
+            );
+            for (let i = 1; i < values.length; i += 1) {
+              computedArray.push(
+                [new Date(values[i].end_date).getFullYear().toString(),
+                  values[i].new_users + computedArray[i - 1][1]],
+              );
+            }
+            collumPeriodTitle = [['Ano', 'Total de Usuários Cadastrados']];
+            break;
         }
-        collumPeriodTitle = [['Dia', 'Total de Usuários Cadastrados']];
-        break;
-      case monthlyKeyWord:
-        computedArray.push(
-          [monthNamesList[(new Date(values[0].end_date)).getMonth()], values[0].new_users],
-        );
-        for (let i = 1; i < values.length; i += 1) {
-          computedArray.push(
-            [monthNamesList[(new Date(values[i].end_date)).getMonth()],
-              values[i].new_users + computedArray[i - 1][1]],
-          );
-        }
-        collumPeriodTitle = [['Mês', 'Total de Usuários Cadastrados']];
-        break;
-      default:
-        computedArray.push(
-          [new Date(values[0].end_date).getFullYear().toString(), values[0].new_users],
-        );
-        for (let i = 1; i < values.length; i += 1) {
-          computedArray.push(
-            [new Date(values[i].end_date).getFullYear().toString(),
-              values[i].new_users + computedArray[i - 1][1]],
-          );
-        }
-        collumPeriodTitle = [['Ano', 'Total de Usuários Cadastrados']];
-        break;
-    }
 
-    setTotalUsersChartDataLastUpdate(values[0].modified);
+        setTotalUsersChartDataLastUpdate(values[0].modified);
+      } else {
+        collumPeriodTitle = [];
+      }
+    } catch (e) {
+    // eslint-disable-next-line no-console
+      console.error(e);
+    }
     setTotalUsersChartData(collumPeriodTitle.concat(computedArray));
     setTotalUsersChartDataLoaded(true);
   }
@@ -257,6 +266,8 @@ function Wikilegis(props) {
 
     if (Array.isArray(values) && values.length) {
       computeTotalOfUsersByPeriod(values, period);
+    } else {
+      computeTotalOfUsersByPeriod(null, period);
     }
   }
 
