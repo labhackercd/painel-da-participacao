6b7f3882e55cf4e7b931731064bc35c7514a8c5d
Joao Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2021-05-19 10:35:51 -0300

    #147 - Adding new alert design

--- src/containers/Audiencias/index.js
+++ src/containers/Audiencias/index.js
@@ -471,6 +471,8 @@ function Audiencias(props) {
         { showCachedDataAlert && (
           <AlertCachedData apiLastCacheMade={apiLastCacheMade} />
         )}
+          <AlertCachedData apiLastCacheMade={apiLastCacheMade} />
+
         <Grid item xs={12}>
           <SectionHeader classes={classes} toolTipText={null} title="Totais no Período" />
         </Grid>
