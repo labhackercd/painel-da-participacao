11e954be177753e12f2592abe786e861f19ac2c4
mlfaa <mlfaa@hotmail.com>
2021-11-29 16:25:35 -0300

    #209 - Removing the underline from the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable react/prop-types */
 import React from 'react';
-import {
-  Typography, Box,
-} from '@material-ui/core';
+import { Typography, Box } from '@material-ui/core';
 import { useStyles } from './style';
 import logoCamaraDosDeputados from '../../assets/images/logos/logo_cd.svg';
 
@@ -15,12 +13,15 @@ export default function Footer() {
       <Box className={classes.footerContainer}>
         <Box className={classes.bannerBox} />
         <Box className={classes.informationsDiv}>
-
           <Box className={classes.informationItemLab}>
-            <Typography className={`${classes.typography} ${classes.informationItemTypographyText}`}>
+            <Typography
+              className={`${classes.typography} ${classes.informationItemTypographyText}`}
+            >
               Versão 1.0 - 2021
             </Typography>
-            <Typography className={`${classes.typography} ${classes.informationItemTypographyText}`}>
+            <Typography
+              className={`${classes.typography} ${classes.informationItemTypographyText}`}
+            >
               LABHacker
             </Typography>
           </Box>
@@ -38,17 +39,28 @@ export default function Footer() {
               href={`${process.env.NEXT_PUBLIC_FALE_CONOSCO_URL}`}
               target="_blank"
               rel="noopener noreferrer"
-              style={{ color: '#F1EA67' }}
+              style={{ color: '#F1EA67', textDecoration: 'none' }}
             >
-              <Typography style={{ fontWeight: 'bold' }} className={`${classes.typography} ${classes.informationItemTypographyText}`}>Fale Conosco</Typography>
+              <Typography
+                style={{ fontWeight: 'bold' }}
+                className={`${classes.typography} ${classes.informationItemTypographyText}`}
+              >
+                Fale Conosco
+              </Typography>
             </a>
-            <Typography className={`${classes.typography} ${classes.informationItemTypographyText}`}>Disque-Câmara:</Typography>
-            <Typography className={`${classes.typography} ${classes.informationItemTypographyText}`}>0800-0-619-619 de 8h às 20h</Typography>
+            <Typography
+              className={`${classes.typography} ${classes.informationItemTypographyText}`}
+            >
+              Disque-Câmara:
+            </Typography>
+            <Typography
+              className={`${classes.typography} ${classes.informationItemTypographyText}`}
+            >
+              0800-0-619-619 de 8h às 20h
+            </Typography>
           </Box>
-
         </Box>
       </Box>
-
     </footer>
   );
 }
