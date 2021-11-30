da7eaf456b0c3114041aa96a1f49839c68c2da95
mlfaa <mlfaa@hotmail.com>
2021-11-30 09:40:22 -0300

    #209 - Setting svg property

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -30,7 +30,7 @@ const CustomLeft = ({ onClick }) => {
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        enable-background="new 0 0 24 24"
+        enableBackground="new 0 0 24 24"
         height="26"
         viewBox="0 0 24 24"
         width="26"
@@ -56,7 +56,7 @@ const CustomRight = ({ onClick }) => {
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        enable-background="new 0 0 24 24"
+        enableBackground="new 0 0 24 24"
         height="26"
         viewBox="0 0 24 24"
         width="26"
