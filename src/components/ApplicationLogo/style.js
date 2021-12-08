4428d442f84a33f1a7ceb8eb086bb02bcf82f277
mlfaa <mlfaa@hotmail.com>
2021-12-08 14:52:33 -0300

    #215 - Changing size logo e-monitor

--- src/components/ApplicationLogo/style.js
+++ src/components/ApplicationLogo/style.js
@@ -3,16 +3,16 @@ import { makeStyles } from '@material-ui/core';
 const useStyles = makeStyles((theme) => ({
   logoClass: {
     cursor: 'pointer',
-    width: '140px',
-    height: '35px',
+    width: '161px',
+    height: '34px',
 
-    [theme.breakpoints.up('md')]: {
+    [theme.breakpoints.up('sm')]: {
       width: '250px',
       height: '55px',
     },
     [theme.breakpoints.up('xl')]: {
-      width: '300px',
-      height: '60px',
+      width: '270px',
+      height: '56px',
     },
   },
 }));
