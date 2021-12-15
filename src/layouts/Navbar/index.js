9aa877cc26372724a55dbada9639009784b84bee
mlfaa <mlfaa@hotmail.com>
2021-12-15 15:07:21 -0300

    #237 - Fixing anchor in mobile menu

--- src/layouts/Navbar/index.js
+++ src/layouts/Navbar/index.js
@@ -181,7 +181,7 @@ export default function PageNavbar(props) {
     return (
       <Toolbar>
         <Drawer
-          anchor="up"
+          anchor="top"
           open={drawerOpen}
           onClose={handleDrawerClose}
           classes={{ paper: drawerMobile }}
