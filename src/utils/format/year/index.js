650145e6d04af9490512d7c875d72be5e428576c
mlfaa <mlfaa@hotmail.com>
2021-12-23 14:23:42 -0300

    fixing month in formatYear function

--- src/utils/format/year/index.js
+++ src/utils/format/year/index.js
@@ -2,11 +2,15 @@ export default function formatYear(searchedText) {
   const date = new Date(searchedText);
   // Removing two digits in year number
   const year = date.getFullYear().toString().substr(-2);
-  const month = date.getMonth() + 1;
+  let month = date.getMonth() + 1;
   let day = date.getDate();
 
   if (day.toString().length === 1) {
     day = `0${day}`;
+  }
+
+  if (month.toString().length === 1) {
+    month = `0${month}`;
   }
 
   return `${day}/${month}/${year}`;
