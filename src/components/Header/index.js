87ec424db3a6c2daa7ad7887909af811bbe3d94e
mlfaa <mlfaa@hotmail.com>
2022-01-18 16:52:14 -0300

    changing mobile visualization header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,7 +10,6 @@ import * as APPLICATION_OPTIONS from '../../settings/applicationOptions/index';
 import * as APPLICATION_CONSTANTS from '../../utils/constants/index';
 
 import { useStyles } from './style';
-import HeaderMobile from '../HeaderMobile';
 
 export default function Header(props) {
   const colors = props.headerColors;
@@ -26,13 +25,23 @@ export default function Header(props) {
     if (event.target.value === '0') {
       setMonth('0');
       setSelectMonthDisabled(true);
+      if (mobileView) {
+        props.handlePeriodChange('0', '0');
+      }
     } else {
       setSelectMonthDisabled(false);
+      if (mobileView) {
+        setMonth('0');
+        props.handlePeriodChange('0', event.target.value);
+      }
     }
   };
 
   const handleChangeMonth = (event) => {
     setMonth(event.target.value);
+    if (mobileView) {
+      props.handlePeriodChange(event.target.value, selectedYear);
+    }
   };
 
   const handleSubmit = () => {
@@ -48,64 +57,58 @@ export default function Header(props) {
       <AppBar position="sticky" elevation={0}>
         <Toolbar>
           <Box width="100%" className={classes.inputOptions}>
-            {!mobileView ? (
-              <>
-                <FormControl id="form-control-year" className={classes.formControl}>
-                  <InputLabel id="year-label" htmlFor="select-year">Ano</InputLabel>
-                  <Select
-                    id="select-year"
-                    labelId="year-label"
-                    value={selectedYear}
-                    native
-                    onChange={handleChangeYear}
-                    input={<InputBase className={classes.inputBase} />}
-                    inputProps={{
-                      classes: {
-                        icon: classes.icon,
-                      },
-                    }}
-                    classes={{
-                      select: classes.select,
-                    }}
-                  >
-                    <option value="0">Todo o Período</option>
-                    {yearsRange.map((item) => <option key={`year${item}`} value={item}>{item}</option>)}
-                  </Select>
-                </FormControl>
-                <FormControl id="form-control-month" className={classes.formControl}>
-                  <InputLabel id="month-label" htmlFor="select-month">Mês</InputLabel>
-                  <Select
-                    id="select-month"
-                    labelId="month-label"
-                    value={month}
-                    native
-                    onChange={handleChangeMonth}
-                    input={<InputBase className={classes.inputBase} />}
-                    classes={{
-                      select: classes.select,
-                    }}
-                    inputProps={{
-                      classes: {
-                        icon: (selectMonthDisabled ? classes.iconDisabled : classes.icon),
-                      },
-                    }}
-                    disabled={selectMonthDisabled}
-                  >
-                    <option value="0">Todos os Meses</option>
-                    {APPLICATION_CONSTANTS.MONTHS_LIST.map((monthItem, indexMonth) => <option key={`month${monthItem}`} value={indexMonth + 1}>{monthItem}</option>)}
-                  </Select>
-                </FormControl>
+            <>
+              <FormControl id="form-control-year" className={classes.formControl}>
+                <InputLabel id="year-label" htmlFor="select-year">Ano</InputLabel>
+                <Select
+                  id="select-year"
+                  labelId="year-label"
+                  value={selectedYear}
+                  native
+                  onChange={handleChangeYear}
+                  input={<InputBase className={classes.inputBase} />}
+                  inputProps={{
+                    classes: {
+                      icon: classes.icon,
+                    },
+                  }}
+                  classes={{
+                    select: classes.select,
+                  }}
+                >
+                  <option value="0">Todo o Período</option>
+                  {yearsRange.map((item) => <option key={`year${item}`} value={item}>{item}</option>)}
+                </Select>
+              </FormControl>
+              <FormControl id="form-control-month" className={`${!mobileView && classes.formControl}`}>
+                <InputLabel id="month-label" htmlFor="select-month">Mês</InputLabel>
+                <Select
+                  id="select-month"
+                  labelId="month-label"
+                  value={month}
+                  native
+                  onChange={handleChangeMonth}
+                  input={<InputBase className={classes.inputBase} />}
+                  classes={{
+                    select: classes.select,
+                  }}
+                  inputProps={{
+                    classes: {
+                      icon: (selectMonthDisabled ? classes.iconDisabled : classes.icon),
+                    },
+                  }}
+                  disabled={selectMonthDisabled}
+                >
+                  <option value="0">Todos os Meses</option>
+                  {APPLICATION_CONSTANTS.MONTHS_LIST.map((monthItem, indexMonth) => <option key={`month${monthItem}`} value={indexMonth + 1}>{monthItem}</option>)}
+                </Select>
+              </FormControl>
+              { !mobileView && (
                 <Button variant="contained" className={classes.filterButton} onClick={handleSubmit}>
                   Filtrar
                 </Button>
-              </>
-            ) : (
-              <HeaderMobile
-                headerColors={colors}
-                handlePeriodChange={props.handlePeriodChange}
-                initialYear={props.initialYear}
-              />
-            )}
+              )}
+            </>
           </Box>
         </Toolbar>
       </AppBar>
